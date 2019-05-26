500e7bcbe4ed661bbd58b35be1439dfa7dc03c10
meimeia <shen2288@126.com>
2019-05-26 18:49:01 +0800

    去掉注释

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-console.log(Platform,99)
+// console.log(Platform,99)
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
